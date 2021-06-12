3be939fc19ecbec3ea2b2e8e3cfbcac3bcfa4c8c
Josh <66690986+cantcodetbh@users.noreply.github.com>
2021-06-12 14:58:22 +0100

    ozzy fixed the search

--- search.js
+++ search.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('inputbox');
     // onClick's logic below:
-    link.addEventListener('keydown', function() {
+    link.addEventListener('keydown', function(e) {
         //need to add
         function searchGoogle() {
-        var one = document.getElementById("inputbox").value;
-        var two = 'https://www.google.com/search?q=' + one;
-        window.location = two;
-    }
+            var one = document.getElementById("inputbox").value;
+            var two = 'https://www.google.com/search?q=' + one;
+            window.open(two , '_blank')
+        }
 
-
+        if (e.key === 'Enter') {
+            searchGoogle()
+            e.currentTarget.value = "";
+            document.getElementById('inputbox').blur();
+        }
     });
 });
